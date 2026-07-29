ff0d71cebda74d398e067d47e2944fea488fdca3
federicopessina <federicopessina.work@gmail.com>
2026-07-29 22:32:20 +0200

    feat: compiler generated ctors

---
+++ CompilerGeneratedFunctions/main.cpp
@@ -0,0 +1,27 @@
+// https://www.youtube.com/watch?v=e8Cw17p_BiU&list=PL5jc9xFGsL8FWtnZBeTqZBbniyw0uHyaH&index=6
+
+// Compiler Generated Functions
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    C++ 03
+    1. default ctor
+    2. copy ctor                (generated only if 5. and 6 not declared)
+    3. copy assignment operator (generated only if 5. and 6 not declared)
+    4. destructor
+
+    c++ 11
+    5. move ctor                (generated only if 2. 3. 4. 6. not declared)
+    6. move assignment operator (generated only if 2. 3. 4. 5. not declared)
+    */
+
+
+    // TODO check examples !!!
+
+    return 0;
+}
